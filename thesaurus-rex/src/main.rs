b955c34307abf2f74f827b8bd28d50908656f433
TyRoXx <tyroxxxx@gmail.com>
2024-04-21 01:28:10 +0200

    Deduplicate potential matches

--- thesaurus-rex/src/main.rs
+++ thesaurus-rex/src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+use std::hash::Hash;
+
 enum RegularLanguage<A> {
     Empty,
     Singleton(A),
@@ -11,19 +14,18 @@ fn new_empty_word<A>() -> RegularLanguage<A> {
     RegularLanguage::<A>::Repetition(Box::new(RegularLanguage::Empty))
 }
 
-fn try_match<'t, A>(language: &RegularLanguage<A>, word: &'t [A]) -> Vec<&'t [A]>
+fn try_match<'t, A: Hash>(language: &RegularLanguage<A>, word: &'t [A]) -> HashSet<&'t [A]>
 where
-    A: core::cmp::PartialEq<A>,
+    A: core::cmp::Eq,
 {
     match language {
-        RegularLanguage::Empty => Vec::new(),
+        RegularLanguage::Empty => HashSet::new(),
         RegularLanguage::Singleton(character) => match word {
-            [head, ..] if (character == head) => vec![word.split_at(1).1],
-            _ => Vec::new(),
+            [head, ..] if (character == head) => HashSet::from([word.split_at(1).1]),
+            _ => HashSet::new(),
         },
         RegularLanguage::Repetition(element) => {
-            // TODO: deduplicate results
-            let mut results = Vec::new();
+            let mut results = HashSet::new();
             let mut stack = vec![vec![word]];
             loop {
                 match &mut stack.last_mut() {
@@ -31,9 +33,9 @@ where
                         let next_match = last_element.pop();
                         match next_match {
                             Some(matched) => {
-                                results.push(matched);
+                                results.insert(matched);
                                 let element_matches: Vec<&'t [A]> = try_match(&element, matched)
-                                    .drain(..)
+                                    .drain()
                                     // avoid infinite loop by discarding empty word matches
                                     .filter(|matched_tail| matched_tail.len() < matched.len())
                                     .collect();
@@ -52,26 +54,26 @@ where
             results
         }
         RegularLanguage::Union(first, second) => {
-            let mut first_match = try_match(first, word);
-            let mut second_match = try_match(second, word);
-            first_match.append(&mut second_match);
-            first_match
+            let mut first_matches = try_match(first, word);
+            let second_matches = try_match(second, word);
+            first_matches.extend(&second_matches);
+            first_matches
         }
         RegularLanguage::Concatenation(first, second) => {
-            let mut results = Vec::new();
+            let mut results = HashSet::new();
             let first_matches = try_match(&first, word);
             for first_match in first_matches {
-                let mut second_matches = try_match(&second, first_match);
-                results.append(&mut second_matches);
+                let second_matches = try_match(&second, first_match);
+                results.extend(&second_matches);
             }
             results
         }
     }
 }
 
-fn is_match<A>(language: &RegularLanguage<A>, word: &[A]) -> bool
+fn is_match<A: Hash>(language: &RegularLanguage<A>, word: &[A]) -> bool
 where
-    A: core::cmp::PartialEq<A>,
+    A: core::cmp::Eq,
 {
     let matches = try_match(language, word);
     matches.iter().find(|element| element.is_empty()).is_some()
@@ -210,6 +212,26 @@ fn consider_non_empty_match() {
     assert!(is_match(&language, &['a', 'a', 'a']));
     assert!(!is_match(&language, &['a', 'a', 'b']));
     assert!(!is_match(&language, &['a', 'a', 'a', 'a']));
+}
+
+#[test]
+fn repeated_ambiguity() {
+    let language = RegularLanguage::<char>::Repetition(Box::new(RegularLanguage::<char>::Union(
+        Box::new(RegularLanguage::<char>::Concatenation(
+            Box::new(RegularLanguage::<char>::Singleton('a')),
+            Box::new(RegularLanguage::<char>::Singleton('a')),
+        )),
+        Box::new(RegularLanguage::<char>::Singleton('a')),
+    )));
+    assert!(is_match(&language, &[]));
+    assert!(is_match(&language, &['a']));
+    assert!(!is_match(&language, &['b']));
+    assert!(is_match(&language, &['a', 'a']));
+    assert!(is_match(&language, &['a'; 10]));
+    assert!(is_match(&language, &['a'; 20]));
+    //TODO: make these fast
+    //assert!(is_match(&language, &['a'; 30]));
+    //assert!(is_match(&language, &['a'; 100]));
 }
 
 fn main() {
