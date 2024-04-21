fa5057ffd075e8af8521a79bd1a62b8399de553c
TyRoXx <tyroxxxx@gmail.com>
2024-04-21 03:02:20 +0200

    Make is_match fast for ambiguous repetitions

--- thesaurus-rex/src/main.rs
+++ thesaurus-rex/src/main.rs
@@ -1,5 +1,9 @@
+#![feature(coroutines, coroutine_trait)]
 use std::collections::HashSet;
 use std::hash::Hash;
+use std::ops::Coroutine;
+use std::ops::CoroutineState;
+use std::pin::Pin;
 
 enum RegularLanguage<A> {
     Empty,
@@ -14,69 +18,124 @@ fn new_empty_word<A>() -> RegularLanguage<A> {
     RegularLanguage::<A>::Repetition(Box::new(RegularLanguage::Empty))
 }
 
-fn try_match<'t, A: Hash>(language: &RegularLanguage<A>, word: &'t [A]) -> HashSet<&'t [A]>
+pub fn gen_to_iter<A, G: Coroutine<Return = (), Yield = A> + Unpin>(
+    gen2: G,
+) -> impl Iterator<Item = A> {
+    CoroutineIter {
+        state: CoroutineIterState::Pending,
+        gen2,
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct CoroutineIter<G: Coroutine<Return = ()>> {
+    state: CoroutineIterState,
+    gen2: G,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum CoroutineIterState {
+    Pending,
+    Empty,
+}
+
+impl<G: Coroutine<Return = ()> + Unpin> Iterator for CoroutineIter<G> {
+    type Item = G::Yield;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.state {
+            CoroutineIterState::Empty => None,
+            CoroutineIterState::Pending => match Pin::new(&mut self.gen2).resume(()) {
+                CoroutineState::Yielded(value) => Some(value),
+                CoroutineState::Complete(_) => {
+                    self.state = CoroutineIterState::Empty;
+                    None
+                }
+            },
+        }
+    }
+}
+
+fn try_match<'t, A: Hash>(
+    language: &'t RegularLanguage<A>,
+    word: &'t [A],
+) -> impl Iterator<Item = &'t [A]> + 't
 where
     A: core::cmp::Eq,
 {
-    match language {
-        RegularLanguage::Empty => HashSet::new(),
-        RegularLanguage::Singleton(character) => match word {
-            [head, ..] if (character == head) => HashSet::from([word.split_at(1).1]),
-            _ => HashSet::new(),
-        },
-        RegularLanguage::Repetition(element) => {
-            let mut results = HashSet::new();
-            let mut stack = vec![vec![word]];
-            loop {
-                match &mut stack.last_mut() {
-                    Some(last_element) => {
-                        let next_match = last_element.pop();
-                        match next_match {
-                            Some(matched) => {
-                                results.insert(matched);
-                                let element_matches: Vec<&'t [A]> = try_match(&element, matched)
-                                    .drain()
-                                    // avoid infinite loop by discarding empty word matches
-                                    .filter(|matched_tail| matched_tail.len() < matched.len())
-                                    .collect();
-                                if !element_matches.is_empty() {
-                                    stack.push(element_matches);
+    gen_to_iter(move || {
+        match language {
+            RegularLanguage::Empty => {}
+            RegularLanguage::Singleton(character) => match word {
+                [head, ..] if (character == head) => {
+                    yield word.split_at(1).1;
+                }
+                _ => {}
+            },
+            RegularLanguage::Repetition(element) => {
+                let mut results = HashSet::new();
+                let mut stack = vec![vec![word]];
+                loop {
+                    match &mut stack.last_mut() {
+                        Some(last_element) => {
+                            let next_match = last_element.pop();
+                            match next_match {
+                                Some(matched) => {
+                                    if results.insert(matched) {
+                                        yield matched;
+                                    }
+                                    let inner_generator = Box::new(try_match(&element, matched));
+                                    let element_matches: Vec<&'t [A]> = inner_generator
+                                        // avoid infinite loop by discarding empty word matches
+                                        .filter(|matched_tail| matched_tail.len() < matched.len())
+                                        .collect();
+                                    if !element_matches.is_empty() {
+                                        stack.push(element_matches);
+                                    }
+                                }
+                                None => {
+                                    stack.pop();
                                 }
                             }
-                            None => {
-                                stack.pop();
-                            }
                         }
+                        None => break,
                     }
-                    None => break,
                 }
             }
-            results
-        }
-        RegularLanguage::Union(first, second) => {
-            let mut first_matches = try_match(first, word);
-            let second_matches = try_match(second, word);
-            first_matches.extend(&second_matches);
-            first_matches
-        }
-        RegularLanguage::Concatenation(first, second) => {
-            let mut results = HashSet::new();
-            let first_matches = try_match(&first, word);
-            for first_match in first_matches {
-                let second_matches = try_match(&second, first_match);
-                results.extend(&second_matches);
+            RegularLanguage::Union(first, second) => {
+                let first_matches = Box::new(try_match(first, word));
+                let mut already_yielded = HashSet::new();
+                for first_match in first_matches {
+                    yield first_match;
+                    already_yielded.insert(first_match);
+                }
+                let second_matches = Box::new(try_match(second, word));
+                for second_match in second_matches {
+                    if already_yielded.contains(second_match) {
+                        continue;
+                    }
+                    yield second_match;
+                }
             }
-            results
+            RegularLanguage::Concatenation(first, second) => {
+                let first_matches = Box::new(try_match(&first, word));
+                for first_match in first_matches {
+                    let second_matches = Box::new(try_match(&second, first_match));
+                    for second_match in second_matches {
+                        yield second_match;
+                    }
+                }
+            }
         }
-    }
+    })
 }
 
 fn is_match<A: Hash>(language: &RegularLanguage<A>, word: &[A]) -> bool
 where
     A: core::cmp::Eq,
 {
-    let matches = try_match(language, word);
-    matches.iter().find(|element| element.is_empty()).is_some()
+    let mut matches = try_match(language, word);
+    matches.find(|element| element.is_empty()).is_some()
 }
 
 #[test]
@@ -229,9 +288,11 @@ fn repeated_ambiguity() {
     assert!(is_match(&language, &['a', 'a']));
     assert!(is_match(&language, &['a'; 10]));
     assert!(is_match(&language, &['a'; 20]));
-    //TODO: make these fast
-    //assert!(is_match(&language, &['a'; 30]));
-    //assert!(is_match(&language, &['a'; 100]));
+    assert!(is_match(&language, &['a'; 30]));
+    assert!(is_match(&language, &['a'; 100]));
+    assert!(is_match(&language, &['a'; 1000]));
+    // TODO: make this fast:
+    // assert!(is_match(&language, &['a'; 10000]));
 }
 
 fn main() {
